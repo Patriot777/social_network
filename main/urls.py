7db3ddba93bca640365a9e78951bdff0f195677a
Patriot777 <zolinbest@gmail.com>
2024-01-13 23:05:47 +0200

    Додав базу даних

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import BooklListCreateView
-
+#hello
 urlpatterns = [
     path('api/your_model/', BooklListCreateView.as_view(), name='book_list_create_view'),
 ]
